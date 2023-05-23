18eac789cc166bdb947dd950ff64d63d8ba6501a
Natik Alam Tahsin <natikalamtahsin@gmail.com>
2023-05-23 14:54:06 +0600

    category subcategory updated

--- src/Pages/Home/Products/Products.jsx
+++ src/Pages/Home/Products/Products.jsx
@@ -5,9 +5,11 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 const Products = () => {
-  const [category, setCategory] = useState("Best Selling");
+  const categories = ["Best Selling", "Fan Favourite", "Top Rated"];
+  const [activeCategory, setActiveCategory] = useState(categories[0]);
+  // const [category, setCategory] = useState("Best Selling");
   const [subCategory, setsSubCategory] = useState([]);
-  const [selectedSubCategory, setSelectedSubCategory] = useState("all");
+  const [activeSubCategory, setActiveSubCategory] = useState("All");
   const [toys, setToys] = useState([]);
 
   useEffect(() => {
@@ -18,27 +20,27 @@ const Products = () => {
 
   useEffect(() => {
     fetch(
-      `https://toy-marketplace-server-eta-weld.vercel.app/category/${category}`
+      `https://toy-marketplace-server-eta-weld.vercel.app/category/${activeCategory}`
     )
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
         setsSubCategory(data.subcategories);
       });
-  }, [category]);
+  }, [activeCategory]);
 
   useEffect(() => {
     fetch(
-      `https://toy-marketplace-server-eta-weld.vercel.app/toys/${category}?subCategory=${selectedSubCategory}`
+      `https://toy-marketplace-server-eta-weld.vercel.app/toys/${activeCategory}?subCategory=${activeSubCategory}`
     )
       .then((res) => res.json())
       .then((data) => setToys(data));
-  }, [category, selectedSubCategory]);
+  }, [activeCategory, activeSubCategory]);
   const handleCategory = (item) => {
-    setSelectedSubCategory("all");
-    setCategory(item);
+    setActiveSubCategory("All");
+    setActiveCategory(item);
   };
-  const categories = ["Best Selling", "Fan Favourite", "Top Rated"];
+
   return (
     <div className="custom-container py-14 ">
       <h1
@@ -67,7 +69,9 @@ const Products = () => {
         {categories.map((item, i) => (
           <button
             key={i}
-            className="button-secondary"
+            className={`font-bruno text-base px-8 py-2 rounded-lg border border-solid border-sky-700 shadow-lg font-semibold tracking-wider text-slate-900  hover:-translate-y-1 transition-all duration-200 ease-in-out ${
+              activeCategory === item ? "bg-slate-200" : "bg-white"
+            }`}
             onClick={() => handleCategory(item)}
           >
             {item}
@@ -83,8 +87,12 @@ const Products = () => {
         {subCategory.map((item, i) => (
           <button
             key={i}
-            className="button-primary"
-            onClick={() => setSelectedSubCategory(item)}
+            className={`sub-category-btn ${
+              activeSubCategory === item
+                ? "active-subcategory-btn"
+                : "deactive-subcategory-btn"
+            }`}
+            onClick={() => setActiveSubCategory(item)}
           >
             {item}
           </button>
